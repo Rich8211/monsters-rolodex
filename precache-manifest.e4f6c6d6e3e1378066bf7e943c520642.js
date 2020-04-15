f7d157fb766bc2f35d7c2ab8305e782995d03008
Rich <Richlee821@gmail.com>
2020-04-14 20:29:02 -0400

    Updates

---
+++ precache-manifest.e4f6c6d6e3e1378066bf7e943c520642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c04b5ff9f8c547bb16b629b4ecb6a1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f389935d20d33e9cf473",
+    "url": "/monsters-rolodex/static/css/main.2891be62.chunk.css"
+  },
+  {
+    "revision": "9afaee7bd521b6bd2ca8",
+    "url": "/monsters-rolodex/static/js/2.877a8798.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters-rolodex/static/js/2.877a8798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f389935d20d33e9cf473",
+    "url": "/monsters-rolodex/static/js/main.255d8031.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
